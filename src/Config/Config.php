f37aad66be852e2ac334e456e75cf72a543bc75b
Xiao Hu Tai <xiao@twokings.nl>
2017-05-19 13:32:36 +0200

    Override `get` function for `Config` class and set better defaults
    Inspired from `\Bolt\Config`

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -16,4 +16,47 @@ class Config extends ParameterBag
     {
         parent::__construct($parameters);
     }
+
+    /**
+     * This function is aken from \Bolt\Config class:
+     * @link https://github.com/bolt/bolt/blob/release/3.2/src/Config.php#L202-L239
+     *
+     * Get a config value, using a path. So the third parameter $deep is ignored.
+     *
+     * For example:
+     * $var = $config->get('general/wysiwyg/ck/contentsCss');
+     *
+     * @param string               $path
+     * @param string|array|boolean $default
+     * @param bool                 $deep
+     *
+     * @return mixed
+     */
+    public function get($path, $default = null, $deep = false)
+    {
+        $path = explode('/', $path);
+
+        // Only do something if we get at least one key.
+        if (empty($path[0]) || !isset($this->parameters[$path[0]])) {
+            return false;
+        }
+
+        $part = & $this->parameters;
+        $value = null;
+
+        foreach ($path as $key) {
+            if (!isset($part[$key])) {
+                $value = null;
+                break;
+            }
+
+            $value = $part[$key];
+            $part = & $part[$key];
+        }
+        if ($value !== null) {
+            return $value;
+        }
+
+        return $default;
+    }
 }
--- src/Service/HierarchicalRoutesService.php
+++ src/Service/HierarchicalRoutesService.php
@@ -94,7 +94,7 @@ class HierarchicalRoutesService
         $this->cache      = $cache;
         $this->logger     = $logger;
 
-        $this->cacheDuration = $this->config->get('cache')['duration'] * 60;
+        $this->cacheDuration = $this->config->get('cache/duration', 10) * 60;
 
         $this->build();
 
@@ -118,7 +118,7 @@ class HierarchicalRoutesService
      */
     public function build($useCache = true)
     {
-        if ($this->config->get('cache')['enabled'] && $useCache && $this->fromCache()) {
+        if ($this->config->get('cache/enabled', true) && $useCache && $this->fromCache()) {
             return;
         }
 
@@ -137,9 +137,12 @@ class HierarchicalRoutesService
             $this->importMenu($menu);
         }
 
-        $this->importRules($this->config->get('rules'));
+        $rules = $this->config->get('rules', []);
+        if (is_array($rules)) {
+            $this->importRules();
+        }
 
-        if ($this->config->get('cache')['enabled']) {
+        if ($this->config->get('cache/enabled', true)) {
             $this->toCache();
         }
     }
